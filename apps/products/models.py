ada0438fd529a895151b1abeaf84e743e6cf203e
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-07-12 09:36:13 -0500

    master

--- apps/products/models.py
+++ apps/products/models.py
@@ -25,10 +25,11 @@ class MeasureUnit(BaseModel):
 
 
 class CategoryProduct(BaseModel):
-    description = models.CharField('Descripcion', max_length=50, blank=False, null=False, unique=True)
+    description = models.CharField('Descripcion', max_length=50, unique=True, blank=False, null=False)
     measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name='Unidad de Medida')
     historical = HistoricalRecords()
 
+    # TODOS LOS MODELOS QUE TENGAN LA PROPIEDAD HISTORICALRECORDS DEBEN TENER EL CODIGO DE LAS LINES 33 A LA 39
     @property
     def _history_user(self):
         return self.changed_by
@@ -38,15 +39,16 @@ class CategoryProduct(BaseModel):
         self.changed_by = value
 
     class Meta:
-        verbose_name = 'Categoria del Producto'
-        verbose_name_plural = 'Categorias de los Productos'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
 
     def __str__(self):
-        return self.description
+        return f'{self.description}'
+
 
 class Indicator(BaseModel):
-    discount_value = models.PositiveSmallIntegerField(default=0)
-    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Indicador de Descuento')
+    descount_value = models.PositiveSmallIntegerField(default=0)
+    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Indicador de Oferta')
     historical = HistoricalRecords()
 
     @property
@@ -58,17 +60,19 @@ class Indicator(BaseModel):
         self.changed_by = value
 
     class Meta:
-        verbose_name = 'Inicador de Descuento'
-        verbose_name_plural = 'Indicadores de Descuento'
+        verbose_name = 'Indicador de Oferta'
+        verbose_name_plural = 'Indicadores de Ofertas'
 
     def __str__(self):
-        return f'Oferta de la categoria{self.category_product} | {self.discount_value}%'
+        return f'Oferta de la Categoria{self.category_product} : { self.descount_value}%'
 
 
 class Product(BaseModel):
     name = models.CharField('Producto', max_length=250, blank=False, null=False, unique=True)
     description = models.TextField('Descripcion del Producto', blank=False, null=False)
     product_image = models.ImageField('Imagen', upload_to='products/', blank=True, null=True)
+    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, null=True)
+    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, null=True)
     historical = HistoricalRecords()
 
     @property
