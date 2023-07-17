ef6dce91a65c2529c3b009b6851d621c7d7dbcde
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-07-16 22:40:23 -0500

    master

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from apps.products.api.views.generic_views import MeasureUnitListView, IndicatorListAPIView, CategoryProductListAPIView
-from apps.products.api.views.products_views import ProductListAPIView, CreateProductAPIView
+from apps.products.api.views.products_views import (
+    ListCreateProductAPIView,
+    ProductRetrieveUpdateDestroyAPIView,
+    # ProductDeleteAPIView,
+    # ProductUpdateAPIView
+)
 
 urlpatterns = [
     path('measure_unit/', MeasureUnitListView.as_view(), name='Measure Unit'),
     path('indicator/', IndicatorListAPIView.as_view(), name='Indicator'),
     path('category_product/', CategoryProductListAPIView.as_view(), name='Product Category'),
-    path('products_list/', ProductListAPIView.as_view(), name='List of Products'),
-    path('create_product/', CreateProductAPIView.as_view(), name='Create Product')
+    path('product/', ListCreateProductAPIView.as_view(), name='Create and List Product'),
+    path('retrieve_update_destroy_product/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(),
+         name='Retrieve Update and Destroy Product'),
+    # path('delete_product/<int:pk>/', ProductDeleteAPIView.as_view(), name='Delete Product'),
+    # path('update_product/<int:pk>/', ProductUpdateAPIView.as_view(), name='Update Product')
 ]
--- apps/products/api/views/products_views.py
+++ apps/products/api/views/products_views.py
@@ -4,13 +4,9 @@ from apps.products.api.serializers.product_serializers import ProductSerializer
 from apps.base.api import GeneralListAPIView
 
 
-class ProductListAPIView(GeneralListAPIView):
+class ListCreateProductAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
-
-
-class CreateProductAPIView(generics.CreateAPIView):
-    serializer_class = ProductSerializer
-
+    queryset = serializer_class.Meta.model.objects.filter(state=True)
     # SOBRE ESCRIBIR UN METODO POST
 
     def post(self, request):
@@ -21,3 +17,68 @@ class CreateProductAPIView(generics.CreateAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state=True)
+        else:
+            return self.get_serializer().Meta.model.objects.filter(id=pk, state=True).first()
+
+    def patch(self, request, pk=None):
+        if self.get_queryset(pk):
+            serializer = self.serializer_class(self.get_queryset(pk))
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'error': 'No existe este Producto'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        if self.get_queryset(pk):
+            serializer = self.serializer_class(self.get_queryset(pk), data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):
+        product = self.get_queryset(pk)
+        if product:
+            product.state = False
+            product.save()
+            return Response({'message': 'Producto Desactivado Correctamente'}, status=status.HTTP_200_OK)
+        return Response({'message': 'No existe este Producto'}, status=status.HTTP_400_BAD_REQUEST)
+
+# class ProductDeleteAPIView(generics.DestroyAPIView):
+#     serializer_class = ProductSerializer
+#
+#     def get_queryset(self):
+#         return self.get_serializer().Meta.model.objects.filter(state=True)
+#
+#     def delete(self, request, pk=None):
+#         product = self.get_queryset().filter(id=pk).first()
+#         if product:
+#             product.state = False
+#             product.save()
+#             return Response({'message': 'Producto Desactivado Correctamente'}, status=status.HTTP_200_OK)
+#         return Response({'message': 'No existe este Producto'}, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ProductUpdateAPIView(generics.UpdateAPIView):
+#     serializer_class = ProductSerializer
+#
+#     def get_queryset(self, pk):
+#         return self.get_serializer().Meta.model.objects.filter(state=True).filter(id=pk).first()
+#
+#     def patch(self, request, pk=None):
+#         if self.get_queryset(pk):
+#             serializer = self.serializer_class(self.get_queryset(pk))
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response({'error': 'No existe este Producto'}, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def put(self, request, pk=None):
+#         if self.get_queryset(pk):
+#             serializer = self.serializer_class(self.get_queryset(pk), data=request.data)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response(serializer.data, status=status.HTTP_200_OK)
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
